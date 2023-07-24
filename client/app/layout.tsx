f91ed7ccb74c3f0bb7dd65aa63c04f7453e3fccc
BTH-IT <hungbienthanh@gmail.com>
2023-07-25 01:36:06 +0700

    add rsuite into project

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import ProviderComponent from '@/components/Provider';
 import ToastComponent from '@/components/ToastComponent';
 import '../css/layouts/main-layout.css';
-import { Metadata } from 'next';
+import 'rsuite-no-reset.min.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
