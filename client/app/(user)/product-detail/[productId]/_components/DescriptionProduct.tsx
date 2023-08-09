ce8cd160de927ec63e93b2507194220baf9b1f09
BTH-IT <hungbienthanh@gmail.com>
2023-08-09 21:17:45 +0700

    fix product detail page

--- client/app/(user)/product-detail/[productId]/_components/DescriptionProduct.tsx
+++ client/app/(user)/product-detail/[productId]/_components/DescriptionProduct.tsx
@@ -4,7 +4,9 @@ const DescriptionProduct = ({ description }: { description: string }) => {
   return (
     <>
       <h4>Mô tả sản phẩm</h4>
-      <div className="product__detail-description">{description}</div>
+      <div className="product__detail-description">
+        {description ? description : 'Đang cập nhật....'}
+      </div>
     </>
   );
 };
